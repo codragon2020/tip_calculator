362ba364844bfa9462b617b75814d793f2e55034
Code JSON <73917308+codragon2020@users.noreply.github.com>
2020-12-06 22:51:47 -0500

    Added the calculateTotal function. Created the newTotal and updated the textContent to show the newTotal

--- script.js
+++ script.js
@@ -8,13 +8,18 @@ function calculateTip(total, tipPercentage) {
     return roundedResult;
 }
   
+function calculateTotal(total, tipAmount) {
+    return parseFloat(total) + parseFloat(tipAmount);
+}
+
 function addTip(event) {
     event.preventDefault();
     var tipPercentage = tipEl.value * .01;
-    console.log(tipPercentage);
     var total = totalEl.value;
     var tipAmount = calculateTip(total, tipPercentage);
+    var newTotal = calculateTotal(tipAmount, total);
     document.querySelector("#tip-amount").textContent = tipAmount;
+    document.querySelector("#new-total").textContent = newTotal.toFixed(2);
 }
 
 submitEl.addEventListener("click", addTip);
