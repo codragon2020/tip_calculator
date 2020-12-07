838628916faf3d1ae5446ce938cf0dccc8f778f3
Code JSON <73917308+codragon2020@users.noreply.github.com>
2020-12-06 22:47:45 -0500

    Created the addTip function that will set the input tip value to a percentage and call the calculateTip function to multiple the total with the tipPercentage

--- script.js
+++ script.js
@@ -3,3 +3,18 @@ var totalEl = document.querySelector("#total");
 var submitEl = document.querySelector("#submit");
 var splitEl = document.querySelector("#split");
 
+function calculateTip(total, tipPercentage) {
+    var roundedResult = (total * tipPercentage).toFixed(2);
+    return roundedResult;
+}
+  
+function addTip(event) {
+    event.preventDefault();
+    var tipPercentage = tipEl.value * .01;
+    console.log(tipPercentage);
+    var total = totalEl.value;
+    var tipAmount = calculateTip(total, tipPercentage);
+    document.querySelector("#tip-amount").textContent = tipAmount;
+}
+
+submitEl.addEventListener("click", addTip);
